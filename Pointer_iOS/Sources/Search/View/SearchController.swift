1246cf8ec76a54494ea1d766e2488c3d616477c4
jisu15-kim <jisu15.kim@gmail.com>
2023-03-19 03:06:41 +0900

    fix: 텍스트필드 Width 디바이스 기반 사이즈로 수정

--- Pointer_iOS/Sources/Search/View/SearchController.swift
+++ Pointer_iOS/Sources/Search/View/SearchController.swift
@@ -18,8 +18,8 @@ class SearchController: BaseViewController {
         tf.backgroundColor = .darkGray
         tf.attributedPlaceholder = NSMutableAttributedString(string: "검색어를 입력하세요.", attributes: [NSAttributedString.Key.foregroundColor: UIColor.pointerGray, NSAttributedString.Key.font: UIFont.notoSansRegular(size: 13)])
         tf.textColor = .white
-        tf.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        tf.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        tf.heightAnchor.constraint(equalToConstant: Device.navigationBarHeight).isActive = true
+//        tf.widthAnchor.constraint(equalToConstant: 300).isActive = true
         tf.addLeftPadding(width: 15)
         tf.layer.cornerRadius = 20
         tf.clipsToBounds = true
@@ -121,8 +121,11 @@ class SearchController: BaseViewController {
     }
     
     private func setupNavi() {
+        let searchBarWidth = Device.width - Device.navigationBarHeight - 55
+        searchBar.widthAnchor.constraint(equalToConstant: searchBarWidth).isActive = true
+        
         let backButtonImage = UIImage(systemName: "chevron.backward")
-        let backButton = UIBarButtonItem.getPointerBarButton(withIconimage: backButtonImage, size: 45, target: self, handler: #selector(backButtonTapped))
+        let backButton = UIBarButtonItem.getPointerBarButton(withIconimage: backButtonImage, size: Device.navigationBarHeight, target: self, handler: #selector(backButtonTapped))
         let titleView = UIBarButtonItem(customView: searchBar)
         
         navigationItem.leftBarButtonItems = [backButton, titleView]
