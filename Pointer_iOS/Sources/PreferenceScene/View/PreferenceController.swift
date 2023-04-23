fe0c5991f35430fe86d2d9846705c1fa37877bec
jisu15-kim <jisu15.kim@gmail.com>
2023-04-23 20:19:23 +0900

    feat: collectionView Configure

--- Pointer_iOS/Sources/PreferenceScene/View/PreferenceController.swift
+++ Pointer_iOS/Sources/PreferenceScene/View/PreferenceController.swift
@@ -9,13 +9,15 @@ import UIKit
 import SnapKit
 
 let cellIdentifier = "PreferenceItemCell"
+let headerIdentifier = "PreferenceItemHeader"
 
-class PreferenceController: UIViewController {
+class PreferenceController: BaseViewController {
     //MARK: - Properties
     let viewModel = PreferenceViewModel()
     lazy var collectionView: UICollectionView = {
         let layout = generateLayout()
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .clear
         return cv
     }()
     
@@ -29,6 +31,7 @@ class PreferenceController: UIViewController {
     //MARK: - Functions
     func setupCollectionView() {
         collectionView.register(PreferenceItemCell.self, forCellWithReuseIdentifier: cellIdentifier)
+        collectionView.register(PreferenceItemHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerIdentifier)
         collectionView.dataSource = self
         collectionView.delegate = self
     }
@@ -36,12 +39,15 @@ class PreferenceController: UIViewController {
     func generateLayout() -> UICollectionViewLayout {
         
         let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(40)))
-//        mainItem.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
         
         let group = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(250)), subitems: [item])
         
         let section = NSCollectionLayoutSection(group: group)
-        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 5, bottom: 10, trailing: 5)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 16, bottom: 40, trailing: 16)
+        
+        let headerItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(40))
+        let headerItem = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerItemSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        section.boundarySupplementaryItems = [headerItem]
         
         let layout = UICollectionViewCompositionalLayout(section: section)
         return layout
@@ -70,6 +76,17 @@ extension PreferenceController: UICollectionViewDataSource, UICollectionViewDele
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as? PreferenceItemCell else { return UICollectionViewCell() }
+        let section = PreferenceModel.SectionType.allCases[indexPath.section]
+        let typeModels = PreferenceModel.allCases.filter { $0.type == section }
+        let type = typeModels[indexPath.row]
+        cell.item = type
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let view = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerIdentifier, for: indexPath) as? PreferenceItemHeader else { return UICollectionReusableView() }
+        let type = PreferenceModel.SectionType.allCases[indexPath.section]
+        view.headerType = type
+        return view
     }
 }
--- Pointer_iOS/Sources/PreferenceScene/View/PreferenceItemHeader.swift
+++ Pointer_iOS/Sources/PreferenceScene/View/PreferenceItemHeader.swift
@@ -1,5 +1,5 @@
 //
-//  PreferenceItemCell.swift
+//  PreferenceItemHeader.swift
 //  Pointer_iOS
 //
 //  Created by 김지수 on 2023/04/23.
@@ -8,9 +8,9 @@
 import UIKit
 import SnapKit
 
-class PreferenceItemCell: UICollectionViewCell {
+class PreferenceItemHeader: UICollectionReusableView {
     //MARK: - Properties
-    var item: PreferenceModel? {
+    var headerType: PreferenceModel.SectionType? {
         didSet {
             configure()
         }
@@ -18,7 +18,7 @@ class PreferenceItemCell: UICollectionViewCell {
     
     var titleLabel: UILabel = {
         let label = UILabel()
-        label.font = .notoSansRegular(size: 16)
+        label.font = .notoSansBold(size: 16)
         label.textColor = .white
         return label
     }()
@@ -26,28 +26,25 @@ class PreferenceItemCell: UICollectionViewCell {
     //MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: .zero)
-        
-//        let color: [UIColor] = [.red, .blue, .gray, .green, .systemIndigo]
-//        backgroundColor = color.randomElement()
         setupUI()
+        backgroundColor = .brown
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
-    //MARK: - Functions
+    //MARK: - functions
     private func setupUI() {
         addSubview(titleLabel)
         titleLabel.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(22)
             $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(22)
         }
     }
     
     private func configure() {
-        guard let item = item else { return }
-        titleLabel.text = item.rawValue
+        guard let type = headerType else { return }
+        titleLabel.text = type.rawValue
     }
 }
