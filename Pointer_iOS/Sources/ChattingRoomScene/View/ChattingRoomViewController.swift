67f702fc929dc905bf116bcb03fc19ff00f20a9f
Hyeonjoonn <phj3046@gmail.com>
2023-05-23 14:39:12 +0900

    add: chattingRoomScene 폴더 & 파일 구성 추가

---
+++ Pointer_iOS/Sources/ChattingRoomScene/View/ChattingRoomViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ChattingRoomViewController.swift
+//  Pointer_iOS
+//
+//  Created by 박현준 on 2023/05/23.
+//
+
+import UIKit
+import SnapKit
+
+class ChattingRoomViewController: BaseViewController {
+
+//MARK: - Properties
+    
+    
+//MARK: - Life cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
