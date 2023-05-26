a062449b4b82d2482ac5aa47dfc8a27a73e39277
Hyeonjoonn <phj3046@gmail.com>
2023-05-26 22:19:28 +0900

    feat: 샌드버드 기본세팅 완료

--- Pointer_iOS/Secret/Secret.swift
+++ Pointer_iOS/Secret/Secret.swift
@@ -11,4 +11,8 @@ struct Secret {
     static let kakaoNativeKey = "96c598921c281722bca7c018eed53133"
     
     static let loginURL = "http://211.176.69.65:8080/auth/login"
+    
+    static let sendbird_App = "53D80BCA-B5D6-49F4-9D0F-6AD226B53F86"
+    static let sendbird_Api = "dc52d659ac4a22650038d85e97ce270b0d27ddd8"
+    static let sendbird_Feign_Url = "https://api-53d80bca-b5d6-49f4-9d0f-6ad226b53f86.sendbird.com/v3"
 }
