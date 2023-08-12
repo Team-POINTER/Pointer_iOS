6efcfbabbf19a9159b840ce300a2692e0fa59ab5
Hyeonjoonn <phj3046@gmail.com>
2023-08-13 02:25:32 +0900

    add: 유저 목록 검색 API Router & NetworkManger로 변경

---
+++ Pointer_iOS/API/HTTP/FriendSearchRouter.swift
@@ -0,0 +1,62 @@
+//
+//  FriendRouter.swift
+//  Pointer_iOS
+//
+//  Created by 박현준 on 2023/08/09.
+//
+
+import Foundation
+import Alamofire
+
+enum FriendSearchRouter {
+    case searchUser // 유저 검색
+    case searchfriend // 친구 검색
+    case searchBlockedFriend // 차단된 친구 검색
+}
+
+extension FriendSearchRouter: HttpRouter {
+
+    var url: String {
+        return baseUrlString + path
+    }
+
+    var baseUrlString: String {
+        return Secret.baseURL
+    }
+
+    var path: String {
+        switch self {
+        case .searchUser:
+            return "/search"
+        case .searchfriend:
+            return "/friend/search"
+        case .searchBlockedFriend:
+            return "/friend/block/search"
+        }
+    }
+
+    var method: HTTPMethod {
+        switch self {
+        case .searchUser:
+            return .post
+        case .searchfriend:
+            return .post
+        case .searchBlockedFriend:
+            return .post
+        }
+    }
+
+    var headers: HTTPHeaders? {
+        guard let accessToken = TokenManager.getUserAccessToken() else { return HTTPHeaders() }
+        return ["Content-Type" : "application/json",
+                "Authorization" : "Bearer \(accessToken)"]
+    }
+
+    var parameters: Parameters? {
+        return nil
+    }
+
+    func body() throws -> Data? {
+        return nil
+    }
+}
